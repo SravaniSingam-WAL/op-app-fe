0c718c7157d56b4cd0d28e29da369b01693a4d7f
amarnadh meda <amaranadh.m@westagilelabs.com>
2024-01-29 13:28:21 +0530

    Add package.json and base path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import Login from './login';
 import Home from './home';
 
+const history = createBrowserHistory({ basename: '/op' });
 function App() {
   return (
-    <Router>
+    <Router history={history} basename="/op">
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
