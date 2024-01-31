526da465fb52c97df68ef0e502ba5484fa8e8423
SravaniSingam-WAL <sravani.s@westagilelabs.com>
2024-01-31 15:06:53 +0530

    added css

--- src/home.js
+++ src/home.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import { getBrandName } from "./utils";
 
 const Home = () => {
+  const brandName = getBrandName()
+ 
   return (
     <div>
-      <h2>Home Pag for OP Application</h2>
+    <span className="brandName">{brandName}</span>
+      <p className="para">Home Page for OP Application</p>
     </div>
   );
 };
